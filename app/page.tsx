26e150555d4bb052658a70fc8ca468bf2dfae6d9
Bagus Erwanto <57589007+volumeee@users.noreply.github.com>
2024-05-19 17:37:12 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import SectionHeader from "./section/sectionHeader";
+import SectionList from "./section/sectionList";
+
+const Page = () => (
+  <div>
+    <SectionHeader />
+    <SectionList />
+  </div>
+);
+
+export default Page;
---
+++ app/section/sectionHeader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SectionHeader() {
+  return (
+    <div className="w-full mt-[60px]">
+      <img alt="header" src="img/header.png" className="" />
+    </div>
+  );
+}
